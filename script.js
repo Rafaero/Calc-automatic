85333d17bc2a50ca98d2150f37667a3be0dd7c7c
Rafaero <rafaelpaivavitor@hotmail.com>
2021-11-09 14:14:07 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function calc(){
+    let questionOne = document.getElementById("one").value
+    let questionTwo = document.getElementById("two").value
+    let questionThree = document.getElementById("three").value
+    let questionFour = document.getElementById("four").value
+    let questionFive = document.getElementById("five").value
+    let questionSix = document.getElementById("six").value
+    let questionSeven = document.getElementById("seven").value
+    let questionEight = document.getElementById("eight").value
+    let questionNine = document.getElementById("nine").value
+    let questionTen = document.getElementById("ten").value
+    let questionEleven = document.getElementById("eleven").value
+
+    let oneNumber = parseFloat(questionOne)
+    let twoNumber = parseFloat(questionTwo)
+    let threeNumber = parseFloat(questionThree)
+    let fourNumber = parseFloat(questionFour)
+    let fiveNumber = parseFloat(questionFive)
+    let sixNumber = parseFloat(questionSix)
+    let sevenNumber = parseFloat(questionSeven)
+    let eightNumber = parseFloat(questionEight)
+    let nineNumber = parseFloat(questionNine)
+    let tenNumber = parseFloat(questionTen)
+    let elevenNumber = parseFloat(questionEleven)
+
+
+
+    let porcetagem = ((oneNumber + twoNumber + threeNumber + fourNumber + fiveNumber + sixNumber + sevenNumber + eightNumber + nineNumber + tenNumber + elevenNumber) / 34.5) * 100;
+    let sum = (oneNumber + twoNumber + threeNumber + fourNumber + fiveNumber + sixNumber + sevenNumber + eightNumber + nineNumber + tenNumber + elevenNumber)
+
+    let resultPorcent = (porcetagem.toFixed(2) + " %")
+
+
+    document.getElementById("show-porcent").innerHTML = resultPorcent
+
+    document.getElementById("show-sum").innerHTML = sum.toFixed(2)
+
+}
